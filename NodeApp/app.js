3fa572309732339727e08a74aed6362ffbe59aff
Kuba Wasiak <qbawasiak@wp.pl>
2019-01-17 15:03:58 +0100

    html

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,15 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '192.168.0.18';
+const hostname = '192.168.0.14';
 // const hostname = '0.0.0.0';
+// const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World! ');
+fs.readFile('index.html', (err, html) => {
+    if (err) {
+        throw err;
+    }
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html');
+        res.write(html);
+        res.end();
+    });
+    
+    server.listen(port, hostname, () => {
+        console.log(`Server started on port ${port}`);
+    })
 });
-
-server.listen(port, hostname, () => {
-    console.log(`Server started on port ${port}`);
-})
