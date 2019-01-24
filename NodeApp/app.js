3fdd08adbe9a7d4d25eeff40c580e371b4a7be37
Wasiak <wasiakj@emea.roche.com>
2019-01-24 17:13:39 +0100

    handlebars tpl engine installed

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -3,10 +3,15 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // app.use((req, res, next) => {
@@ -17,11 +22,11 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404page.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 // const server = http.createServer(app);
 // server.listen(3000);
