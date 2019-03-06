9d1926f430d5d462ff53f64bdcafa9f3ef89feba
Wasiak <wasiakj@emea.roche.com>
2019-03-06 22:06:36 +0100

    ejs tpl

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,33 +1,24 @@
-// const http = require('http');
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// app.use((req, res, next) => {
-//     console.log('in the middleware!');
-//     next(); // alows the request to continue to the next middleware in line
-// });
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
-// const server = http.createServer(app);
-// server.listen(3000);
-app.listen(3000); // same as 2 lines above
+
+app.listen(3000);
