fed7a006f601074fe2223800b740bc5fff16087b
Wasiak <wasiakj@emea.roche.com>
2019-05-16 14:32:56 +0200

    models added

--- my-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is test', 'https://live.staticflickr.com/4891/45165261135_69cb589907_b.jpg'),
+    new Recipe('A test recipe', 'this is test', 'https://live.staticflickr.com/4891/45165261135_69cb589907_b.jpg')
+  ];
 
   constructor() { }
 
